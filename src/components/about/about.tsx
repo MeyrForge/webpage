9e38b2cfcd271877fd21ae15600fc7152876698f
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2025-09-24 21:31:25 -0300

    Fix styles

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,10 +4,12 @@ export default function About() {
   return (
     <section className="about" id="about">
       <h2>Sobre nosotros</h2>
-      <p>💻 Somos dos devs mobile que salimos de la UNLaM con el título en mano y muchas ganas de codear el mundo.</p>
-      <p>🚀 Amamos la tecnología, los dados de 20 caras y todo lo que tenga olor a innovación.</p>
-      <p>👾 Cuando no estamos trabajando en IT, nos juntamos a crear proyectos propios que nos diviertan y que, de paso, hagan la vida un poco más fácil.</p>
-      <p>✨ Este portafolio es nuestro laboratorio de ideas, donde mezclamos código, creatividad y café hasta sacar cosas que nos enorgullecen.</p>
+      <div className="about-content">
+        <p>💻 Somos dos devs mobile que salimos de la UNLaM con el título en mano y muchas ganas de codear el mundo.</p>
+        <p>🚀 Amamos la tecnología, los dados de 20 caras y todo lo que tenga olor a innovación.</p>
+        <p>👾 Cuando no estamos trabajando en IT, nos juntamos a crear proyectos propios que nos diviertan y que, de paso, hagan la vida un poco más fácil.</p>
+        <p>✨ Este portafolio es nuestro laboratorio de ideas, donde mezclamos código, creatividad y café hasta sacar cosas que nos enorgullecen.</p>
+      </div>
     </section>
   );
 }
